d332d438813f36bd582aaeeb7b818f32ec3374de
eshwar rachala <eshwar.appdev@gmail.com>
2017-05-18 23:06:06 -0400

    updated

--- src/app/components/demo.component.ts
+++ src/app/components/demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ChartService } from '@angular/nvd3';
 
 @Component({
@@ -8,14 +8,56 @@ import { ChartService } from '@angular/nvd3';
 
 export class DemoComponent implements OnInit {
   private nv: any;
-  constructor(private srvc: ChartService) {
-    this.nv = srvc.getnvD3();
-    // tslint:disable-next-line:no-debugger
-    debugger;
+  private d3: any;
+  @ViewChild('chart') private chartContainer: ElementRef;
+  private height: number;
+  private width: number;
+  private margin = { top: 5, right: 40, bottom: 20, left: 120 };
+
+  private data = [
+    {
+      'title': 'Revenue', 'subtitle': 'US$, in thousands',
+      'ranges': [150, 225, 300], 'measures': [220], 'markers': [250],
+      'markerLines': [270]
+    },
+    {
+      'title': 'Order Size', 'subtitle': 'US$, average',
+      'ranges': [350, 500, 600], 'measures': [100], 'markers': [550],
+      'markerLines': [530]
+    },
+    {
+      'title': 'Satisfaction', 'subtitle': 'out of 5',
+      'ranges': [3.5, 4.25, 5], 'measures': [3.2, 4.7],
+      'markers': [4.4], 'markerLines': [3.8]
+    }
+  ];
+
+  constructor(private service: ChartService) {
+    this.nv = service.getnvD3();
+    this.d3 = service.getD3();
   }
 
   ngOnInit() {
     // tslint:disable-next-line:no-debugger
-    // debugger;
+  //  debugger;
+    const element = this.chartContainer.nativeElement;
+    this.width = element.clientWidth;
+    this.height = 80;
+
+    const chart = this.nv.models.bulletChart()
+      .width(this.width - this.margin.right - this.margin.left)
+      .height(this.height - this.margin.top - this.margin.bottom);
+
+    const vis = this.d3.select(element)
+      .selectAll('svg')
+      .data(this.data)
+      .enter()
+      .append('svg')
+      .attr('class', 'bullet nvd3')
+      .attr('width', this.width)
+      .attr('height', this.height);
+
+
+    vis.transition().duration(1000).call(chart);
   }
 }
