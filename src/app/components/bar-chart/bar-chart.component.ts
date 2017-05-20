4364ca85543ed76e1155efab6026eddb11ec26fc
eshwar rachala <eshwar.appdev@gmail.com>
2017-05-20 08:48:29 -0400

    updated

--- src/app/components/bar-chart/bar-chart.component.ts
+++ src/app/components/bar-chart/bar-chart.component.ts
@@ -1,15 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { ChartService } from 'ngnvd3';
 
 @Component({
   moduleId: module.id,
   selector: 'app-bar-chart',
-  template: '<div class="gallery with-transitions" #chart>bar</div>'
+  template: '<div class="gallery with-transitions" id="chart1"><svg></svg></div>'
 })
 export class BarChartComponent implements OnInit {
+  private nv: any;
+  private d3: any;
+  @ViewChild('chart') private chartContainer: ElementRef;
+  private height: number;
+  private width: number;
+  private margin = { top: 5, right: 40, bottom: 20, left: 120 };
 
-  constructor() { }
+  private data: any;
+
+
+  constructor(private service: ChartService) {
+    this.nv = service.getnvD3();
+    this.d3 = service.getD3();
+    this.data = [
+      {
+        key: 'Cumulative Return',
+        values: [
+          {
+            'label': 'A',
+            'value': 29.765957771107
+          },
+          {
+            'label': 'B',
+            'value': 0
+          },
+          {
+            'label': 'C',
+            'value': 32.807804682612
+          },
+          {
+            'label': 'D',
+            'value': 196.45946739256
+          },
+          {
+            'label': 'E',
+            'value': 0.19434030906893
+          },
+          {
+            'label': 'F',
+            'value': 98.079782601442
+          },
+          {
+            'label': 'G',
+            'value': 13.925743130903
+          },
+          {
+            'label': 'H',
+            'value': 5.1387322875705
+          }
+        ]
+      }
+    ];
+  }
 
   ngOnInit() {
+    const nv = this.nv;
+    const d3 = this.d3;
+    const data = this.data;
+    nv.addGraph(function () {
+      const chart = nv.models.discreteBarChart()
+        .x(function (d) { return d.label })
+        .y(function (d) { return d.value })
+        .staggerLabels(true)
+        .showValues(true)
+        .duration(250)
+        ;
+      d3.select('#chart1 svg')
+        .datum(data)
+        .call(chart);
+
+      nv.utils.windowResize(chart.update);
+      return chart;
+    });
+
+
 
   }
 
