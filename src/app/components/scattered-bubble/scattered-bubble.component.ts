3d7921bf29de7331ff338d67eb695715be936b54
eshwar rachala <eshwar.appdev@gmail.com>
2017-05-21 00:01:19 -0400

    updated scattered plot

--- src/app/components/scattered-bubble/scattered-bubble.component.ts
+++ src/app/components/scattered-bubble/scattered-bubble.component.ts
@@ -23,41 +23,37 @@ export class ScatteredBubbleChartComponent implements OnInit {
  const chart = nv.models.scatterChart()
                 .showDistX(true)
                 .showDistY(true)
-                .transitionDuration(350)
-                .color(d3.scale.category10().range());
+                .useVoronoi(true)
+                .color(d3.scale.category10().range())
+                .duration(300);
 
 
     // Axis settings
     chart.xAxis.tickFormat(d3.format('.02f'));
     chart.yAxis.tickFormat(d3.format('.02f'));
 
-    // We want to show shapes other than circles.
-    chart.scatter.onlyCircles(false);
-
-    const myData = randomData(4, 40);
-
     d3.select('#chart svg')
-    .datum(myData)
+    .datum(randomData(4, 40))
     .call(chart);
 
     nv.utils.windowResize(chart.update);
 
     function randomData(groups, points) {
-       const data = [],
+    const data: Array<{key: any, values: any}> = [],
       shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
       random = d3.random.normal();
 
       for (let i = 0; i < groups; i++) {
         data.push({ key: 'Group ' + i, values: [] });
-      }
 
-       for (let j = 0; j < points; j++) {
-            data[j].values.push({
+        for (let j = 0; j < points; j++) {
+            data[i].values.push({
               x: random()
             , y: random()
             , size: Math.random()
             , shape: (Math.random() > 0.95) ? shapes[j % 6] : 'circle'
             });
+      }
       }
 
       return data;
