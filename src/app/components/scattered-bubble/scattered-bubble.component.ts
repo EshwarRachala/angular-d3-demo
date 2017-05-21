5afa6f21a8757bb32003c9d332171b6874cd3ea9
eshwar rachala <eshwar.appdev@gmail.com>
2017-05-20 23:35:02 -0400

    updated

--- src/app/components/scattered-bubble/scattered-bubble.component.ts
+++ src/app/components/scattered-bubble/scattered-bubble.component.ts
@@ -1,90 +1,66 @@
-
 import { Component, OnInit } from '@angular/core';
 import { ChartService } from 'ngnvd3';
 
 @Component({
   selector: 'app-scattered-bubble-chart',
-  template: `<div class="gallery with-transitions" id="chart1">
+  template: `<div class="gallery with-transitions" id="chart">
                 <svg height="500"></svg>
              </div>`
 })
 export class ScatteredBubbleChartComponent implements OnInit {
   private nv: any;
   private d3: any;
-  private data: any;
 
   constructor(private service: ChartService) {
     this.nv = service.getnvD3();
     this.d3 = service.getD3();
-
-    this.data = sinAndCos();
-
-
-    function sinAndCos() {
-      const sin = [], sin2 = [],
-        cos = [];
-
-      // Data is represented as an array of {x,y} pairs.
-      for (let i = 0; i < 100; i++) {
-        sin.push({ x: i, y: Math.sin(i / 10) });
-        sin2.push({ x: i, y: Math.sin(i / 10) * 0.25 + 0.5 });
-        cos.push({ x: i, y: .5 * Math.cos(i / 10) });
-      }
-
-      // Line chart data should be sent as an array of series objects.
-      return [
-        {
-          values: sin,      // values - represents the array of {x,y} data points
-          key: 'Sine Wave', // key  - the name of the series.
-          color: '#ff7f0e'  // color - optional: choose your own line color.
-        },
-        {
-          values: cos,
-          key: 'Cosine Wave',
-          color: '#2ca02c'
-        },
-        {
-          values: sin2,
-          key: 'Another sine wave',
-          color: '#7777ff',
-          area: true      // area - set to true if you want this line to turn into a filled area chart.
-        }
-      ];
-    }
   }
 
   ngOnInit() {
     const nv = this.nv;
     const d3 = this.d3;
-    const data = this.data;
 
-    const chart = nv.models.lineChart()
-      .options({
-        duration: 300,
-        useInteractiveGuideline: true
-      })
-      ;
-    chart.xAxis
-      .axisLabel('Time (s)')
-      .tickFormat(d3.format(',.1f'))
-      .staggerLabels(true)
-      ;
-    chart.yAxis
-      .axisLabel('Voltage (v)')
-      .tickFormat(function (d) {
-        if (d == null) {
-          return 'N/A';
-        }
-        return d3.format(',.2f')(d);
-      })
-      ;
+ const chart = nv.models.scatterChart()
+                .showDistX(true)
+                .showDistY(true)
+                .transitionDuration(350)
+                .color(d3.scale.category10().range());
 
-    d3.select('#chart1 svg')
-      .datum(this.data)
-      .call(chart);
 
+    // Axis settings
+    chart.xAxis.tickFormat(d3.format('.02f'));
+    chart.yAxis.tickFormat(d3.format('.02f'));
+
+    // We want to show shapes other than circles.
+    chart.scatter.onlyCircles(false);
+
+    const myData = randomData(4, 40);
+
+    d3.select('#chart svg')
+    .datum(myData)
+    .call(chart);
 
     nv.utils.windowResize(chart.update);
-  }
 
+    function randomData(groups, points) {
+       const data = [],
+      shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
+      random = d3.random.normal();
+
+      for (let i = 0; i < groups; i++) {
+        data.push({ key: 'Group ' + i, values: [] });
+      }
+
+       for (let j = 0; j < points; j++) {
+            data[i].values.push({
+              x: random()
+            , y: random()
+            , size: Math.random()
+            , shape: (Math.random() > 0.95) ? shapes[j % 6] : 'circle'
+            });
+      }
+
+      return data;
+    }
+  }
 }
