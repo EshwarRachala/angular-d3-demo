1eb46903e94a82a715c05f4c0c759d37b5ff08a8
eshwar rachala <eshwar.appdev@gmail.com>
2017-05-25 01:22:32 -0400

    updated

--- src/app/components/demo/demo.component.ts
+++ src/app/components/demo/demo.component.ts
@@ -2,45 +2,102 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
-  template: '<pie-chart [data]="data" [height]="height"></pie-chart>'
+  template: `<hmb-chart [data]="data" [height]="height"></hmb-chart>`
 })
 export class DemoComponent implements OnInit {
 
-  height = 400;
+  height = 600;
   data = [
     {
-      'label': 'One',
-      'value': 29.765957771107
+      'key': 'Series 1',
+      'color': '#d67777',
+      'values': [
+        {
+          'label': 'Group A',
+          'value': -1.8746444827653
+        },
+        {
+          'label': 'Group B',
+          'value': -8.0961543492239
+        },
+        {
+          'label': 'Group C',
+          'value': -0.57072943117674
+        },
+        {
+          'label': 'Group D',
+          'value': -2.4174010336624
+        },
+        {
+          'label': 'Group E',
+          'value': -0.72009071426284
+        },
+        {
+          'label': 'Group F',
+          'value': -0.77154485523777
+        },
+        {
+          'label': 'Group G',
+          'value': -0.90152097798131
+        },
+        {
+          'label': 'Group H',
+          'value': -0.91445417330854
+        },
+        {
+          'label': 'Group I',
+          'value': -0.055746319141851
+        }
+      ]
     },
     {
-      'label': 'Two',
-      'value': 0
-    },
-    {
-      'label': 'Three',
-      'value': 32.807804682612
-    },
-    {
-      'label': 'Four',
-      'value': 196.45946739256
-    },
-    {
-      'label': 'Five',
-      'value': 0.19434030906893
-    },
-    {
-      'label': 'Six',
-      'value': 98.079782601442
-    },
-    {
-      'label': 'Seven',
-      'value': 13.925743130903
-    },
-    {
-      'label': 'Eight',
-      'value': 5.1387322875705
+      'key': 'Series 2',
+      'color': '#4f99b4',
+      'values': [
+        {
+          'label': 'Group A',
+          'value': 25.307646510375
+        },
+        {
+          'label': 'Group B',
+          'value': 16.756779544553
+        },
+        {
+          'label': 'Group C',
+          'value': 18.451534877007
+        },
+        {
+          'label': 'Group D',
+          'value': 8.6142352811805
+        },
+        {
+          'label': 'Group E',
+          'value': 7.8082472075876
+        },
+        {
+          'label': 'Group F',
+          'value': 5.259101026956
+        },
+        {
+          'label': 'Group G',
+          'value': 0.30947953487127
+        },
+        {
+          'label': 'Group H',
+          'value': 0
+        },
+        {
+          'label': 'Group I',
+          'value': 0
+        }
+      ]
     }
   ];
+
+  click(event) {
+    this.height = 600;
+    event.preventDefault();
+  }
   constructor() { }
 
   ngOnInit() {
